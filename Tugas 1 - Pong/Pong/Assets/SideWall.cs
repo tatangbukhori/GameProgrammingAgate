a5511179ad51de9f950e60d514056220bf4aa1b2
tatangbukhori <tatangb909@gmail.com>
2021-09-08 16:58:21 +0700

    Tugas 1 Game Programming

---
+++ Tugas 1 - Pong/Pong/Assets/SideWall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerControl player;
+
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        if (anotherCollider.name == "Ball")
+        {
+            player.IncrementScore();
+
+            if (player.Score < gameManager.maxScore)
+            {
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+
+            }
+        }
+    }
+    [SerializeField]
+    private GameManager gameManager;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
